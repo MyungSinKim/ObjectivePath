708172190f925874093fd2de4f07c049dd8135c5
Jonathan Wight <jwight@mac.com>
2012-05-21 13:40:12 -0700

    Initial Commit

---
+++ ObjectPath/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjectPath
+//
+//  Created by Jonathan Wight on 5/21/12.
+//  Copyright (c) 2012 Jonathan Wight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
